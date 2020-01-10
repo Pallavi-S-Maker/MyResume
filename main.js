c72e28f0f505adfa6e5fe323fd68689d44002a78
PALLAVI.S <Pallavi-S-Maker>
2020-01-10 10:00:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+const fruit=["apple","bannana","grape"];
+//console.log(fruit);
+
+//console.log(fruit[1]);
+
+//fruit[1]='mango';
+//console.log(fruit);
+
+//fruit.push('strawberry');
+//console.log(fruit);
+
+
+//fruit.unshift('orange');
+//console.log(fruit);
+
+//fruit.pop();
+//console.log(fruit);
+
+//console.log(fruit.indexOf('bannana'));
+
+const person={
+                name:'mani',
+                age:25,
+                hob:['music','sports','browsing'],
+                height:'4',
+                weight:'46',
+                address:{
+                    taluk:'udupi',
+                    city:'brahmavar',
+                    state:'karnataka'}
+            }
+//console.log(person);
+
+//console.log(person.name);
+
+//console.log(person.hob[1]);
+
+//console.log(person.address.city);
+
+
+
+const todo=[
+    {
+    id:1,
+    text:'work for home',
+    isComplete:false
+    },
+
+    {
+        id:2,
+        text:'work for college',
+        isComplete:false
+        },
+
+        {
+            id:3,
+            text:'work for it',
+            isComplete:false
+            }
+        ]
+
+for(let deeksha of todo)
+{
+    console.log(deeksha.text);
+}
+
+//console.log(todo[2].text);
+
+//console.log(JSON.stringify(todo));
+
+//home work for
+//for(let i=0;i<=10;i++)
+//{
+ //   console.log('for loop number:${i}');
+//}
+
+//while
+//let i=0
+//while(i<=10)
+//{
+  //  console.log('for loop number:${i}');
+  //  i++;
+//}
